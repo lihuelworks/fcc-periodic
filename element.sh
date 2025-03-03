49cbc9b5ff9df6f863b252d6aeb369e2d893a764
Mathias Lihuel Gomez Governa <lihuelworks@gmail.com>
2025-03-03 22:15:52 +0000

    refactor: uppercased input var

--- element.sh
+++ element.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
-input=$1
+INPUT=$1
 SQL_QUERY="SELECT * FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number WHERE "
-if [[ -z $input ]];then
+if [[ -z $INPUT ]];then
   echo "Please provide an element as an argument."
 fi
 
-# Check if the input is a number
-if [[ $input =~ ^-?[0-9]+([.][0-9]+)?$ ]]; then
+# Check if the INPUT is a number
+if [[ $INPUT =~ ^-?[0-9]+([.][0-9]+)?$ ]]; then
     echo "It's a number."
-# Check if the input is a single character
-elif [[ ${#input} -eq 1 ]]; then
+# Check if the INPUT is a single character
+elif [[ ${#INPUT} -eq 1 ]]; then
     echo "It's a single character."
 # Otherwise, it's a string
 else
