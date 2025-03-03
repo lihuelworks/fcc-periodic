c5f11af839e0f498b1365cbc56beed4b9254423e
Mathias Lihuel Gomez Governa <lihuelworks@gmail.com>
2025-03-03 22:15:23 +0000

    feat: initial control flow

--- element.sh
+++ element.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
-if [[ -z $1 ]];then
+input=$1
+SQL_QUERY="SELECT * FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number WHERE "
+if [[ -z $input ]];then
   echo "Please provide an element as an argument."
+fi
+
+# Check if the input is a number
+if [[ $input =~ ^-?[0-9]+([.][0-9]+)?$ ]]; then
+    echo "It's a number."
+# Check if the input is a single character
+elif [[ ${#input} -eq 1 ]]; then
+    echo "It's a single character."
+# Otherwise, it's a string
+else
+    echo "It's a string."
 fi
